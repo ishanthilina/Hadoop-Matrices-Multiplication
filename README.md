a8dd5589abd58204fbae85ca519683867584dc51
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2018-04-08 13:39:40 +1000

    Updated readme and test.sh

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 Matrix-Multiplication
 =====================
 
-How to run..
+Assumptions
+-----------
 
+- Hadoop is installed in `/usr/local/hadoop`. If not Set the `HADOOP_HOME` variable in the beginning on the `test.sh` file.
 
-1. Pack the code into jar including all the dependency libraries or pom.
+Running the Code
+----------------
 
-2. hadoop jar MM.jar matrix.MatrixMultiplication data.txt Output
- 
----> data.txt contains sample input.
+Run the `test.sh` file. It will do the following tasks.
 
----> output contains the result of matrix multiplication.
+- Set Hadoop libraries to the classpath (if not set) 
+- Cleanup previous build/calculation outputs
+- Compile the source code
+- Create a JAR to be executed in Hadoop
+- Run the JAR in Hadoop
+- Check if the output is correct
--- test.sh
+++ test.sh
@@ -1,37 +1,34 @@
 #!/usr/bin/env bash
 
+HADOOP_HOME=/usr/local/hadoop
+
 ##Check if Hadoop path is set
 if [ -z "$HADOOP_CLASSPATH" ]; then
-    echo "Setting HADOOP_CLASSPATH"
-    HADOOP_CLASSPATH=$(/usr/local/hadoop/bin/hadoop classpath)
+    echo -e "\e[42mSetting HADOOP_CLASSPATH\e[0m"
+    HADOOP_CLASSPATH=$(${HADOOP_HOME}/bin/hadoop classpath)
 fi
 
-echo "Cleanup previous builds..."
+echo -e "\e[42mCleanup previous builds...\e[0m"
 rm -r ./matrix/
 rm -r ./Output/
 #create dir if not exists
 mkdir -p matrix
 
-echo "Compiling...."
+echo -e "\e[42mCompiling....\e[0m"
 if javac -classpath ${HADOOP_CLASSPATH}  -d matrix/ ./src/*.java; then
-    echo "Compilation Successful...!"
+    echo -e "\e[42mCompilation Successful...!\e[0m"
 else
     echo "Compilation Failed"
     exit 1
 fi
 
 ls -la ./matrix/
-#if test `find "./matrix/MatrixMultiplication.class" -mmin +120`
-#then
-#    exit 1
-#fi
 
-echo "Creating Jar..."
+echo -e "\e[42mCreating Jar...\e[0m"
 jar -cvf MM.jar -C matrix/ .
 
-
-echo "Run Hadoop..."
-/usr/local/hadoop/bin/hadoop jar MM.jar MatrixMultiplication data.txt Output
+echo -e "\e[42mRun Hadoop...\e[0m"
+${HADOOP_HOME}/bin/hadoop jar MM.jar MatrixMultiplication data.txt Output
 
 #Compare files
 cmp --silent "./Expected/1.txt" "./Output/part-r-00000" || echo -e "\033[31;7mOutput is different than expected\e[0m";
